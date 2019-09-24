1a9b9694bc13e9beeb8bf9a6a439e98009028ca0
liboyi <liboyi@moyi365.com>
2019-09-24 18:44:48 +0800

    加一库

---
+++ robot/robot.h
@@ -0,0 +1,19 @@
+//
+//  robot.h
+//  robot
+//
+//  Created by ek on 2019/9/24.
+//  Copyright © 2019 ek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for robot.
+FOUNDATION_EXPORT double robotVersionNumber;
+
+//! Project version string for robot.
+FOUNDATION_EXPORT const unsigned char robotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <robot/PublicHeader.h>
+
+
---
+++ robot/robot.swift
@@ -0,0 +1,10 @@
+//
+//  robot.swift
+//  robot
+//
+//  Created by ek on 2019/9/24.
+//  Copyright © 2019 ek. All rights reserved.
+//
+
+import Foundation
+public func inc(_ i : Int)->Int{return i + 1}
